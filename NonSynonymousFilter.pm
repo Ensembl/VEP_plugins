d9c22165b80359350a355e131d7a4f913d6c58e7
grsr <gritchie@gmail.com>
2011-12-01 10:20:20 +0000

    add license and documentation

--- NonSynonymousFilter.pm
+++ NonSynonymousFilter.pm
@@ -1,3 +1,34 @@
+=head1 LICENSE
+                                                                                                                     
+ Copyright (c) 1999-2011 The European Bioinformatics Institute and                                                   
+ Genome Research Limited.  All rights reserved.                                                                      
+                                                                                                                     
+ This software is distributed under a modified Apache license.                                                       
+ For license details, please see
+
+   http://www.ensembl.org/info/about/code_licence.html                                                               
+                                                                                                                     
+=head1 CONTACT                                                                                                       
+
+ Graham Ritchie <grsr@ebi.ac.uk>
+    
+=cut
+
+=head1 NAME
+
+ NonSynonymousFilter
+
+=head1 SYNOPSIS
+
+ mv NonSynonymousFilter.pm ~/.vep/Plugins
+ perl variant_effect_predictor.pl -i variations.vcf --plugin NonSynonymousFilter
+
+=head1 DESCRIPTION
+
+ A simple example VEP filter plugin that limits output to non-synonymous variants
+
+=cut
+
 package NonSynonymousFilter;
 
 use strict;
