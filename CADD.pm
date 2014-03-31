32e30b40bb4ba4b6e905d4e9b0d4bcdc99a74966
William McLaren <wm2@ebi.ac.uk>
2014-03-31 12:18:31 +0100

    A plugin to retrieve and add CADD scores
    CADD is a tool for scoring the deleteriousness of single nucleotide
    variants as well as insertion/deletions variants in the human genome.

---
+++ CADD.pm
@@ -0,0 +1,141 @@
+=head1 LICENSE
+
+ Copyright (c) 1999-2013 The European Bioinformatics Institute and                                                   
+ Genome Research Limited.  All rights reserved.                                                                      
+
+ This software is distributed under a modified Apache license.                                                       
+ For license details, please see
+
+   http://www.ensembl.org/info/about/code_licence.html                                                               
+
+=head1 CONTACT                                                                                                       
+
+ Will McLaren <wm2@ebi.ac.uk>
+    
+=cut
+
+=head1 NAME
+
+ CADD
+
+=head1 SYNOPSIS
+
+ mv CADD.pm ~/.vep/Plugins
+ perl variant_effect_predictor.pl -i variations.vcf --plugin CADD,/path/to/CADD/whole_genome_SNVs.tsv.gz
+
+=head1 DESCRIPTION
+
+ A VEP plugin that retrieves CADD scores for single nucleotide variants from a
+ tabix-indexed CADD data file.
+ 
+ Please cite the CADD publication alongside the VEP if you use this resource:
+ http://www.ncbi.nlm.nih.gov/pubmed/24487276
+ 
+ The tabix utility must be installed in your path to use this plugin. The CADD
+ data file can be downloaded from
+ http://cadd.gs.washington.edu/download/
+ 
+=cut
+
+package CADD;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
+
+use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
+
+use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
+
+sub new {
+  my $class = shift;
+  
+  my $self = $class->SUPER::new(@_);
+  
+  # test tabix
+  die "ERROR: tabix does not seem to be in your path\n" unless `which tabix 2>&1` =~ /tabix$/;
+  
+  # get CADD file
+  my $file = $self->params->[0];
+  
+  # remote files?
+  if($file =~ /tp\:\/\//) {
+    my $remote_test = `tabix $file 1:1-1 2>&1`;
+    if($remote_test && $remote_test !~ /get_local_version/) {
+      die "$remote_test\nERROR: Could not find file or index file for remote annotation file $file\n";
+    }
+  }
+
+  # check files exist
+  else {
+    die "ERROR: CADD file $file not found\n" unless -e $file;
+    die "ERROR: Tabix index file $file\.tbi not found - perhaps you need to create it first?\n" unless -e $file.'.tbi';
+  }
+  
+  $self->{file} = $file;
+  
+  return $self;
+}
+
+sub feature_types {
+  return ['Feature','Intergenic'];
+}
+
+sub get_header_info {
+  my $self = shift;
+  return {
+    CADD_PHRED => 'PHRED-like scaled CADD score',
+    CADD_RAW   => 'Raw CADD score'
+  }
+}
+
+sub run {
+  my ($self, $tva) = @_;
+  
+  my $vf = $tva->variation_feature;
+  
+  # only for SNVs
+  return {} unless $vf->{start} eq $vf->{end} && $vf->{allele_string} =~ /[ACGT](\/[ACGT])+/;
+  
+  # get allele, reverse comp if needed
+  my $allele = $tva->variation_feature_seq;
+  reverse_comp(\$allele) if $vf->{strand} < 0;
+  
+  return {} unless $allele =~ /^[ACGT]$/;
+  
+  my $pos_string = sprintf("%s:%i-%i", $vf->{chr}, $vf->{start}, $vf->{end});
+  
+  my %cadd_data;
+  
+  # cached?
+  if(defined($self->{cache}) && defined($self->{cache}->{$pos_string})) {
+    %cadd_data = %{$self->{cache}->{$pos_string}};
+  }
+  
+  # read from file
+  else {
+    open TABIX, sprintf("tabix %s %s |", $self->{file}, $pos_string);
+    
+    while(<TABIX>) {
+      chomp;
+      s/\r$//g;
+      my @split = split /\t/;
+      
+      $cadd_data{$split[3]} = {
+        CADD_RAW   => $split[4],
+        CADD_PHRED => $split[5]
+      };
+    }
+    
+    close TABIX;
+  }
+  
+  # overwrite cache
+  $self->{cache} = {$pos_string => \%cadd_data};
+  
+  return defined($cadd_data{$allele}) ? $cadd_data{$allele} : {};
+}
+
+1;
+
