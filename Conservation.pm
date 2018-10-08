fc9aec7034a8857b91374e900154c9841e139103
aparton <aparton@ebi.ac.uk>
2018-10-08 15:32:42 +0100

    Updates to conservation plugin

--- Conservation.pm
+++ Conservation.pm
@@ -28,7 +28,10 @@ limitations under the License.
 =head1 SYNOPSIS
 
  mv Conservation.pm ~/.vep/Plugins
- ./vep -i variations.vcf --plugin Conservation,GERP_CONSERVATION_SCORE,mammals
+ 
+ ./vep -i variations.vcf --plugin Conservation,mammals
+ ./vep -i variations.vcf --plugin Conservation,/path/to/bigwigfile.bw
+ ./vep -i variations.vcf --plugin Conservation,database,GERP_CONSERVATION_SCORE,mammals
 
 =head1 DESCRIPTION
 
@@ -43,23 +46,71 @@ limitations under the License.
  position will be returned, and for insertions the average score of
  the 2 flanking bases will be returned.
 
- The plugin requires the ensembl-compara API module to be installed;
- see http://www.ensembl.org/info/docs/api/index.html
+ The plugin uses the ensembl-compara API module (optional, see http://www.ensembl.org/info/docs/api/index.html)
+ or obtains data directly from BigWig files (optional, see ftp://ftp.ensembl.org/pub/current_compara/conservation_scores/)
 
 =cut
-
 package Conservation;
 
 use strict;
 use warnings;
 
-use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Slice;
+use Bio::EnsEMBL::IO::Parser::BigWig;
+use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
+use Net::FTP;
 
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new(@_);
+    
+    if(scalar(@{$self->params}) == 0){
+      warn('No input parameters found for Conservation plugin');
+      return $self;
+    }
+    
+    $self->{use_database} = $self->params->[0] eq 'database';
+
+    if($self->{use_database}){
+      shift($self->params);
+      my $params = $self->params;
+
+      # REST API passes 1 as first param
+      shift @$params if $params->[0] && $params->[0] eq '1';
+
+      $self->{method_link_type} = $params->[0] || 'GERP_CONSERVATION_SCORE';
+      $self->{species_set}  = $params->[1] || 'mammals';
+
+      my $config = $self->{config};
+      my $reg = 'Bio::EnsEMBL::Registry';
+
+      # reconnect to DB without species param
+      if($config->{host}) {
+          $reg->load_registry_from_db(
+              -host       => $config->{host},
+              -user       => $config->{user},
+              -pass       => $config->{password},
+              -port       => $config->{port},
+              -db_version => $config->{db_version},
+              -no_cache   => $config->{no_slice_cache},
+          );
+      }
+
+      my $mlss_adap = $config->{mlssa} ||= $reg->get_adaptor('Multi', 'compara', 'MethodLinkSpeciesSet')
+          or die "Failed to connect to compara database\n";
+
+      $self->{mlss} = $mlss_adap->fetch_by_method_link_type_species_set_name($self->{method_link_type}, $self->{species_set})
+          or die "Failed to fetch MLSS for ".$self->{method_link_type}." and ".$self->{species_set}."\n";
+
+      $self->{cs_adap} = $config->{cosa} ||= $reg->get_adaptor('Multi', 'compara', 'ConservationScore')
+          or die "Failed to fetch conservation adaptor\n";
+    }
+    return $self;
+}
 sub version {
-    return '2.3';
+    return '3.0';
 }
 
 sub feature_types {
@@ -71,52 +122,74 @@ sub get_header_info {
     my $self = shift;
     
     return {
-        Conservation => "The conservation score for this site (method_link_type=\"".
-            $self->{method_link_type}."\", species_set=\"".$self->{species_set}."\")",
+        Conservation => "The conservation score for this site"
     };
 }
 
-sub new {
-    my $class = shift;
+sub run {
+  my ($self, $tva) = @_;
+  
+  #We don't want to remove any old functionality, so we allow users to directly access the database if they so choose
 
-    my $self = $class->SUPER::new(@_);
+  return db_run($self, $tva) if $self->{use_database};
+  my $filename;
+  return {} if (scalar(@{$self->params}) eq 0);
 
-    my $params = $self->params;
+  #if it doesn't look like the user has given an FTP link or a file, try and find the correct data
+  if(@{$self->params}[0] !~ /ftp.ensembl.org/ && not -f @{$self->params}[0])
+  {
+    my $FTP_URL  ||= "ftp://ftp.ensembl.org/pub/current_compara/conservation_scores/";
+    my $FTP_USER = 'anonymous';  
 
-    # REST API passes 1 as first param
-    shift @$params if $params->[0] && $params->[0] eq '1';
-
-    $self->{method_link_type} = $params->[0] || 'GERP_CONSERVATION_SCORE';
-    $self->{species_set}  = $params->[1] || 'mammals';
-
-    my $config = $self->{config};
-    my $reg = 'Bio::EnsEMBL::Registry';
-
-    # reconnect to DB without species param
-    if($config->{host}) {
-        $reg->load_registry_from_db(
-            -host       => $config->{host},
-            -user       => $config->{user},
-            -pass       => $config->{password},
-            -port       => $config->{port},
-            -db_version => $config->{db_version},
-            -no_cache   => $config->{no_slice_cache},
-        );
+    $FTP_URL =~ m/(ftp:\/\/)?(.+?)\/(.+)/;  
+    my $ftp = Net::FTP->new($2, Passive => 1) or die "ERROR: Could not connect to FTP host $FTP_URL\n$@\n";
+    $ftp->login($FTP_USER) or die "ERROR: Could not login as $FTP_USER\n$@\n";
+    $ftp->binary();
+    foreach my $sub(split /\//, $3) {
+      $ftp->cwd($sub) or die "ERROR: Could not change directory to $sub\n$@\n";
     }
+    my @files = $ftp->ls;
+    my @dir_to_enter = grep(/@{$self->params}[0]/, @files);
+    if(scalar(@dir_to_enter) ne 1)
+    {
+      warn('Unable to find matching data on FTP site');
+      return {};
+    }
+  
+    my $species = $self->config->{species};
+    my $group = shift(@dir_to_enter);
+    $ftp->cwd($group);
+    @files = $ftp->ls;
+    @dir_to_enter = grep(/$species/, @files);
+    if(scalar(@dir_to_enter) ne 1)
+    {
+      warn('Unable to find matching data on FTP site');
+      return {};
+    }
+    $filename = $FTP_URL . $group . '/' . shift(@dir_to_enter);
+  }
+  else{
+    $filename = @{$self->params}[0] if scalar(@{$self->params});
+  }
+  
+  #Parse and strip out the expected info from the BigWig file
+  my $parser = Bio::EnsEMBL::IO::Parser::BigWig->open($filename);
+    
+  my $vf = $tva->variation_feature;
+  my $allele = $tva->variation_feature_seq;
 
-    my $mlss_adap = $config->{mlssa} ||= $reg->get_adaptor('Multi', 'compara', 'MethodLinkSpeciesSet')
-        or die "Failed to connect to compara database\n";
+  unless($parser){
+    warn ("No BigWig file found for plugin Conservation \n"); 
+    return {};
+  }
 
-    $self->{mlss} = $mlss_adap->fetch_by_method_link_type_species_set_name($self->{method_link_type}, $self->{species_set})
-        or die "Failed to fetch MLSS for ".$self->{method_link_type}." and ".$self->{species_set}."\n";
+  $parser->seek($vf->{chr}, $vf->{start} - 1, $vf->{end});
+  $parser->next;  
 
-    $self->{cs_adap} = $config->{cosa} ||= $reg->get_adaptor('Multi', 'compara', 'ConservationScore')
-        or die "Failed to fetch conservation adaptor\n";
-
-    return $self;
+  return $parser->get_raw_score ? { Conservation => sprintf("%.3f", $parser->get_raw_score)} : {};
 }
 
-sub run {
+sub db_run {
     my ($self, $bvfoa) = @_;
 
     my $bvf = $bvfoa->base_variation_feature;
@@ -219,4 +292,3 @@ sub run {
 }
 
 1;
-
