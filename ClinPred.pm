1ebee662d76da20e77cb9b7b217c6ed39a855bf5
Ola Austine <olaaustine@ebi.ac.uk>
2021-05-07 13:24:00 +0100

    Comments on how to tabix the ClinPred file

--- ClinPred.pm
+++ ClinPred.pm
@@ -26,6 +26,7 @@ limitations under the License.
  adds the CLINPRED(pathogenicity) score for variants to VEP output.
 
  Please cite the CLINPRED publication alongside the VEP if you use this resource:
+https://www.sciencedirect.com/science/article/pii/S000292971830271
 
 CLINPRED SCORES CAN BE DOWNLOADED FROM 
 https://sites.google.com/site/clinpred/products-services
@@ -35,6 +36,7 @@ https://sites.google.com/site/clinpred/products-services
  tabix -f -s 1 -b 2 -e 2 ClinPred.txt.gz 
 
  The tabix utility must be installed in your path to use this plugin.
+ Check https://github.com/samtools/htslib.git for instructions.
 
 
 
@@ -51,6 +53,7 @@ use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp)
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin);
 
+# this is a method from the base class BaseVepPlugin 
 sub new {
   my $class = shift;
   
@@ -63,17 +66,17 @@ sub new {
 
   return $self;
 }
-
+# this is a method from the base class BaseVepPlugin 
 sub feature_types {
   return ['Transcript'];
 
 }
-
+# this is a method from the base class BaseVepPlugin 
 sub get_header_info {
      # a bit confused about what name to call this since, it is not an abbreviation 
      return { ClinPred => "AN ENSEMBLE MACHINE LEARNING PREDICTION TOOL" };
 }
-
+# this is a method from the base class BaseVepPlugin 
 sub run{
      my($self, $tva) = @_;
 
@@ -89,9 +92,21 @@ sub run{
      my $vf = $tva->variation_feature();
      # get allele here from the vcf file 
      my $allele = $tva->variation_feature_seq();
+     return {} unless $allele =~ /^[ACGT]$/;
+
+     my $res = grep{
+          $_->{alt} eq $allele &&
+          $_->{start} eq $vf->{start} &&
+          $_->{end} eq $vf->{end} && } 
+          
+          @{self->get_data($vf->{chr}, $vf=>{$start}, $vf->{end})};
+
+     return $res ? $res->{$result} : {};
+
+
 }
 
-
+# this is a method from the base class BaseVepTabixPlugin 
 sub parse_data {
      # we are parsing data using the header of the ClinPred.txt.gz file 
      # parse data is a method called from the base class BaseVepTabixPlugin
@@ -117,3 +132,5 @@ sub get_start {
 sub get_end {
   return $_[1]->{end};
 }
+
+1;
