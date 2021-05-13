101f4c92dbc982faf731c36050f5899724f1d600
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2021-05-13 12:47:22 +0100

    Implemented review comments

--- ClinPred.pm
+++ ClinPred.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016-2020] EMBL-European Bioinformatics Institute
+Copyright [2016-2021] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -15,51 +15,47 @@ limitations under the License.
 =cut
 
 =head1 NAME
-
-     CLINPRED
+ ClinPred
 
 =head1 SYNOPSIS
- mv Clinpred.pm ~/.vep/Plugins
- ./vep -i variations.vcf --plugin CLINPRED,/path/to/Clinpred/data.txt.gz
+ 
+ mv ClinPred.pm ~/.vep/Plugins
+ ./vep -i variations.vcf --plugin ClinPred,/path/to/ClinPred/data.txt.gz
+
 =head1 DESCRIPTION
  This is a plugin for the Ensembl Variant Effect Predictor (VEP) that
- adds the CLINPRED(pathogenicity) score for variants to VEP output.
+ adds pre-calculated scores from ClinPred.
+ ClinPred is a prediction tool to identify disease-relevant nonsynonymous variants.
 
- Please cite the CLINPRED publication alongside the VEP if you use this resource:
-https://www.sciencedirect.com/science/article/pii/S000292971830271
-
-CLINPRED SCORES CAN BE DOWNLOADED FROM 
-https://sites.google.com/site/clinpred/products-services
- and can be tabix-processed by:
+ This Plugin is only available for GRCh37
  
- for GRCh37:
+ Please cite the ClinPred publication alongside the VEP if you use this resource:
+ https://www.sciencedirect.com/science/article/pii/S0002929718302714?via%3Dihub 
+
+ ClinPred scores can be downloaded from 
+ https://sites.google.com/site/clinpred/products-services
+ 
+ The following steps are neccessary to tabix the ClinPred.txt.gz file before running the plugin:
  gzip -d ClinPred.txt.gz # to unzip the text file 
  cat ClinPred.txt | tr " " "\t" > ClinPred_tabbed.tsv # to change the file to a tabbed delimited file 
-sed '1s/.*/#&/'  ClinPred_tabbed.tsv > tabbed_ClinPred.tsv  #to add a # in the first line of the file 
-sed '1s/C/c' tabbed_ClinPred.tsv > ClinPred_tabbed.tsv # to convert the Chr to chr 
-bgzip ClinPred_tabbed.tsv # to gunzip the file 
+ sed '1s/.*/#&/'  ClinPred_tabbed.tsv > tabbed_ClinPred.tsv  #to add a # in the first line of the file 
+ sed '1s/C/c' tabbed_ClinPred.tsv > ClinPred_tabbed.tsv # to convert the Chr to chr 
+ bgzip ClinPred_tabbed.tsv # to gunzip the file 
 
  tabix -f -s 1 -b 2 -e 2 ClinPred.txt.gz 
 
  The tabix utility must be installed in your path to use this plugin.
  Check https://github.com/samtools/htslib.git for instructions.
 
-
-
 =cut
+
 package ClinPred;
  
-
 use strict;
 use warnings; 
 
-
-
-use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
-
 use Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin;
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin);
-
 
 sub new {
   my $class = shift;
@@ -68,9 +64,11 @@ sub new {
 
   $self->expand_left(0);
   $self->expand_right(0);
-
+  
   $self->get_user_params();
+  die("ERROR: ClinPred input file not specified or found!\n") unless defined($self->params->[0]) && -e $self->params->[0];
 
+  $self->add_file($self->params->[0]);
   return $self;
 }
 
@@ -80,49 +78,36 @@ sub feature_types {
 }
 
 sub get_header_info {
-     # a bit confused about what name to call this since, it is not an abbreviation 
-     return { ClinPred => "CLINVAR PREDICTION TOOL" };
+  return { ClinPred => "Prediction tool to identify disease-relevant nonsynonymous single-nucleotide variants" };
 }
  
 sub run{
-     my ($self, $tva) = @_;
-
-
-     return {} unless grep {$_->SO_term eq 'missense_variant'} @{$tva->get_all_OverlapConsequences};
-
-     my $vf = $tva->variation_feature;
+  my ($self, $tva) = @_;
+  return {} unless grep {$_->SO_term eq 'missense_variant'} @{$tva->get_all_OverlapConsequences};
+  
+  my $vf = $tva->variation_feature;
+  my $allele = $tva->variation_feature_seq;
+  return {} unless $allele =~ /^[ACGT]$/;
+  my ($res) = grep{
+    $_->{alt} eq $allele &&
+    $_->{start} eq $vf->{start} &&
+    $_->{end} eq $vf->{end} 
+    } @{$self->get_data($vf->{chr}, $vf->{start}, $vf->{end})};
     
-     my $allele = $tva->variation_feature_seq;
-     return {} unless $allele =~ /^[ACGT]$/;
-
-     my ($res) = grep{
-          $_->{alt} eq $allele &&
-          $_->{start} eq $vf->{start} &&
-          $_->{end} eq $vf->{end} } 
-          
-          @{$self->get_data($vf->{chr}, $vf->{start}, $vf->{end})};
-
-     return $res ? $res->{result} : {};
-
-
+  return $res ? $res->{result} : {};
 }
 
-
 sub parse_data {
-     
-
-     my ($self, $line) = @_;
-   
-     my ($chr, $start, $ref, $alt, $ClinPred_score ) = split("\t", $line);
-
-     return {
-          start => $start,
-          end => $start,
-          alt => $alt,
-          result => {
-               CLINPRED => $ClinPred_score,
-          }
-           };
+  my ($self, $line) = @_;
+  my ($chr, $start, $ref, $alt, $ClinPred_score ) = split("\t", $line);
+  return {
+    start => $start,
+    end => $start,
+    alt => $alt,
+    result => {
+      ClinPred => $ClinPred_score,
+     }
+        };
 }
 
 sub get_start {
