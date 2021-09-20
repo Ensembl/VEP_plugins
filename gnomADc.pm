1808d2e6b955aec41b1ac090617d9f1e37954ccb
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2021-09-20 14:32:35 +0100

    Updating the documentation of the gnomADc plugin for the v3.1

--- gnomADc.pm
+++ gnomADc.pm
@@ -36,27 +36,33 @@ limitations under the License.
 
  https://gnomad.broadinstitute.org/downloads
  To download the gnomad genomes coverage file in TSV format:
-  wget https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1/coverage/genomes/gnomad.genomes.coverage.summary.tsv.bgz --no-check-certificate
+  wget https://storage.googleapis.com/gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.summary.tsv.bgz --no-check-certificate
+
 
  To download the gnomad exomes coverage file in TSV format: 
-  wget https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1/coverage/exomes/gnomad.exomes.coverage.summary.tsv.bgz --no-check-certificate
+  wget https://storage.googleapis.com/gcp-public-data--gnomad/release/3.0.1/coverage/exomes/gnomad.exomes.r3.0.1.coverage.summary.tsv.bgz --no-check-certificate
+
 
  The coverage summary files must be processed and Tabix indexed before
  use by this plugin. 
 
  The following steps are necessary to tabix the gnomad genomes coverage file :
- mv gnomad.genomes.coverage.summary.tsv.bgz gnomad.genomes.r2.1.gz
- gunzip gnomad.genomes.r2.1.gz
- sed '1s/.*/#&/'  gnomad.genomes.r2.1 > gnomad.genomes.tabbed.tsv
- bgzip gnomad.genomes.tabbed.tsv
- tabix -s 1 -b 2 -e 2 gnomad.genomes.tabbed.tsv.gz
+ mv gnomad.genomes.r3.0.1.coverage.summary.tsv.bgz gnomad.r3.0.1.gz
+ gunzip gnomad.r3.0.1.gz
+ sed '1s/.*/#&/' gnomad.r3.0.1 > gnomad.genomesv3.tabbed.tsv
+ sed '1s/locus/chr\tpos/' gnomad.genomesv3.tabbed.tsv > gnomad.ch.genomesv3.tabbed.tsv
+ sed 's/:/\t/g' gnomad.ch.genomesv3.tabbed.tsv > gnomad.genomesv3.tabbed.tsv
+ bgzip  gnomad.genomesv3.tabbed.tsv 
+ tabix -s 1 -b 2 -e 2 gnomad.genomesv3.tabbed.tsv.gz
 
  The following steps are neccessary to tabix the gnomad exomes coverage file :
- mv gnomad.exomes.coverage.summary.tsv.bgz gnomad.exomes.r2.1.gz
- gunzip gnomad.exomes.r2.1.gz
- sed '1s/.*/#&/'  gnomad.exomes.r2.1 > gnomad.exomes.tabbed.tsv
- bgzip gnomad.exomes.tabbed.tsv
- tabix -s 1 -b 2 -e 2 gnomad.exomes.tabbed.tsv.gz
+ mv gnomad.exomes.r3.0.1.coverage.summary.tsv.bgz gnomad.r3.0.1.gz
+ gunzip gnomad.r3.0.1.gz
+ sed '1s/.*/#&/' gnomad.r3.0.1 > gnomad.exomesv3.tabbed.tsv
+ sed '1s/locus/chr\tpos/' gnomad.exomesv3.tabbed.tsv > gnomad.ch.exomesv3.tabbed.tsv
+ sed 's/:/\t/g' gnomad.ch.exomesv3.tabbed.tsv > gnomad.exomesv3.tabbed.tsv
+ bgzip  gnomad.exomesv3.tabbed.tsv 
+ tabix -s 1 -b 2 -e 2 gnomad.exomesv3.tabbed.tsv.gz
 
  By default, the output field prefix is 'gnomAD'. However if the input file's
  basename is 'gnomADg' (genomes) or 'gnomADe' (exomes), then these values are
