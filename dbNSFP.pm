20c46f9ea8f0d5efe4a3c73a5cd78e60a74966f8
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2022-04-07 12:41:44 +0100

    Warn instead of dying

--- dbNSFP.pm
+++ dbNSFP.pm
@@ -468,10 +468,21 @@ sub run {
       my @refine_fields = grep {defined($data->{$_}) && defined($self->{cols}->{$_})} @{$self->{transcript_specific_fields}};
 
       foreach my $key(@refine_fields) {
+	next if $data->{$key} eq '.';
+
         my @split = split(';', $data->{$key});
-        die("ERROR: Transcript index out of range") if $tr_index > $#split;
-        die("ERROR: Number of transcript IDs does not match number of data entries for field $key") if scalar @split != scalar @tr_ids;
-        $data->{$key} = $split[$tr_index];
+
+	if($tr_index > $#split) {
+          warn("ERROR: Transcript index out of range for field $key\n");
+	  next;
+        }
+
+	if(scalar @split != scalar @tr_ids) {
+	  warn("ERROR: Number of transcript IDs does not match number of data entries for field $key\n");
+	  next;
+        }
+	
+	$data->{$key} = $split[$tr_index];
       }
     }
     last;
