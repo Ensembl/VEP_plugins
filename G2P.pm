8ebac8eb05bb9c46bf0e41f7795a1ad7549eaf8f
olaaustine <olaaustine@ebi.ac.uk>
2022-07-20 16:22:23 +0100

    Changing documentation

--- G2P.pm
+++ G2P.pm
@@ -69,8 +69,8 @@ limitations under the License.
                          VCF collections contain the following populations: 
                          topmed : TOPMed (available for GRCh37 and GRCh38).
                          uk10k : ALSPAC, TWINSUK (available for GRCh37 and GRCh38).
-                         gnomADe & gnomADg - gnomADe:AFR, gnomADe:ALL, gnomADe:AMR, gnomADe:ASJ, gnomADe:EAS, gnomADe:FIN, gnomADe:NFE, gnomADe:OTH, gnomADe:SAS (for GRCh37).
-                         gnomADe_r2.1.1 & gnomADg_v3.1.2 - gnomADg:AFR, gnomADg:ALL, gnomADg:AMR, gnomADg:ASJ, gnomADg:EAS, gnomADg:FIN, gnomADg:NFE, gnomADg:OTH (for GRCh38).
+                         gnomADe & gnomADe_r2.1.1 - gnomADe:AFR, gnomADe:ALL, gnomADe:AMR, gnomADe:ASJ, gnomADe:EAS, gnomADe:FIN, gnomADe:NFE, gnomADe:OTH, gnomADe:SAS (for GRCh37 and GRCh38 respectively).
+                         gnomADg & gnomADg_v3.1.2 - gnomADg:AFR, gnomADg:ALL, gnomADg:AMR, gnomADg:ASJ, gnomADg:EAS, gnomADg:FIN, gnomADg:NFE, gnomADg:OTH (for GRCh37 and GRCh38 respectively).
                          Need to use af_from_vcf paramter to use this option. 
  default_af            : default frequency of the input variant if no frequency data is
                          found (0). This determines whether such variants are included;
