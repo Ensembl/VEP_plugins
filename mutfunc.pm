a2768431950eda45e15c7b808f91d3ec4e4e3f97
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2022-08-03 09:28:23 +0100

    Added all 5 analysis types; formatted output

--- mutfunc.pm
+++ mutfunc.pm
@@ -83,6 +83,7 @@ limitations under the License.
 
 package mutfunc;
 
+use Data::Dumper;
 use strict;
 use warnings;
 use DBI;
@@ -91,34 +92,56 @@ use Digest::MD5 qw(md5_hex);
 use List::MoreUtils qw(first_index);
 
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
-use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
+use Bio::EnsEMBL::Variation::Utils::Sequence qw(get_matched_variant_alleles);
+use Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin;
 
-use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
-
+use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin);
 
 my @ALL_AAS = qw(A C D E F G H I K L M N P Q R S T V W Y);
 
-# dispatch table for frunctions that parse item values
-my $parse_item_value = {
-  motif => \&parse_motif,
-  tfbs  => \&parse_tfbs
+my $field_order = {
+  motif => ["elm", "lost"],
+  int   => ["evidence", "dG_wt", "ddG", "dG_wt_sd", "dG_mt_sd", "ddG_sd"],
+  mod   => ["dG_wt", "ddG", "dG_wt_sd", "dG_mt_sd", "ddG_sd"],
+  exp   => ["dG_wt", "ddG", "dG_wt_sd", "dG_mt_sd", "ddG_sd"],
+  tfbs  => ["impact", "tf", "downstream", "g_strand", "s_strand", "wt_score", "mt_score", "ic_diff", "cells"]
 };
 
 sub new {
   my $class = shift;
   
   my $self = $class->SUPER::new(@_);
+
+  $self->expand_left(0);
+  $self->expand_right(0);
   
   my $param_hash = $self->params_to_hash();
 
-  die "ERROR: db is not specified which is a mandatory parameter\n" unless defined $param_hash->{db};
+  $self->{tfbs} = 1 if $param_hash->{tfbs} || $param_hash->{all};
 
-  $self->{db} = $param_hash->{db};
+  die "ERROR: tfbs file not specified but tfbs output is enabled\n" if ( (defined $self->{tfbs}) && !(defined $param_hash->{file}) );
+  $self->{file} = $param_hash->{file};
+
+  $self->add_file($self->{file}) if defined $self->{file};
 
   $self->{motif} = 1 if $param_hash->{motif} || $param_hash->{all};
   $self->{int} = 1 if $param_hash->{int} || $param_hash->{all};
   $self->{mod} = 1 if $param_hash->{mod} || $param_hash->{all};
   $self->{exp} = 1 if $param_hash->{exp} || $param_hash->{all};
+
+  die "ERROR: db is not specified but some of the options enabled require it\n" if ( 
+    ( (defined $self->{motif}) || 
+      (defined $self->{int}) || 
+      (defined $self->{mod}) || 
+      (defined $self->{exp}) 
+    ) && 
+    !(defined $param_hash->{db}) 
+  );
+  $self->{db} = $param_hash->{db};
+
+  if( ($self->{config}->{output_format} eq "json") || $self->{config}->{rest}){
+    $self->{output_json} = 1;
+  }
 
   $self->{initial_pid} = $$;
 
@@ -126,21 +149,21 @@ sub new {
 }
 
 sub feature_types {
-  return ['Transcript'];
+  return ['Transcript', 'Intergenic'];
 }
 
 sub get_header_info {
-  my $self = shift;
+  my ($self) = shift;
   
   my %header;
 
   $header{"mutfunc_motif"} = "Nonsynonymous mutations impact on linear motif from mutfunc db. ".
-  "Output fields are seperated by comma and include: ".
+  "Output fields are separated by ',' (or '&' for vcf format) and include: ".
   "elm - ELM accession of the linear motif, ".
   "lost - '1' if the mutation causes the motif to be lost and '0' otherwise" if defined $self->{motif};
 
   $header{"mutfunc_int"} = "Interaction interfaces destabilization analysis from mutfunc db. ".
-  "Output fields are seperated by comma and include: ".
+  "Output fields are separated by ',' (or '&' for vcf format) and include: ".
   "evidence - 'EXP' for experimental model and 'MDL' for homology models and 'MDD' for domain-domain homology models, ".
   "dG_wt - reference interface energy (kcal/mol), ".
   "dG_mt - mutated interface energy (kcal/mol), ".
@@ -150,7 +173,7 @@ sub get_header_info {
   "ddG_sd - ddG standard deviation (kcal/mol), " if defined $self->{int};
 
   $header{"mutfunc_mod"} = "Protein structure destabilization analysis (homology models) from mutfunc db. ".
-  "Output fields are seperated by comma and include: ".
+  "Output fields are separated by ',' (or '&' for vcf format) and include: ".
   "dG_wt - reference structure energy (kcal/mol), ".
   "dG_mt - mutated structure energy (kcal/mol), ".
   "ddG - change in structure stability between mutated and reference structure (kcal/mol) mutations where ddG >= 2 kcal/mol can be considered deleterious, ".
@@ -159,13 +182,25 @@ sub get_header_info {
   "ddG_sd - ddG standard deviation (kcal/mol), " if defined $self->{mod};
 
   $header{"mutfunc_exp"} = "Protein structure destabilization analysis (experimental models) from mutfunc db. ".
-  "Output fields are seperated by comma and include: ".
+  "Output fields are separated by ',' (or '&' for vcf format) and include: ".
   "dG_wt - reference structure energy (kcal/mol), ".
   "dG_mt - mutated structure energy (kcal/mol), ".
   "ddG - change in structure stability between mutated and reference structure (kcal/mol) mutations where ddG >= 2 kcal/mol can be considered deleterious, ".
   "dG_wt_sd - dG_wt standard deviation (kcal/mol), ".
   "dG_mt_sd - dG_mt standard deviation (kcal/mol), ".
   "ddG_sd - ddG standard deviation (kcal/mol), " if defined $self->{exp};
+
+  $header{"mutfunc_tfbs"} = "Transcription binding sites disruption analysis from mutfunc db. ".
+  "Output fields are separated by ',' (or '&' for vcf format) and include: ".
+  "impact -  1 if mutation is expected to disrupt the TFBS and 0 otherwise, ".
+  "tf - transcription factor predicted to bind this binding site, ".
+  "downstream - gene downstream from this TFBS, ".
+  "g_strand - strand containing the downstream gene, ".
+  "s_strand - strand containing binding site, ".
+  "wt_score - binding score of the transcription factor to the wildtype site, ".
+  "mt_score - binding score of the transcription factor to the mutated site".
+  "ic_diff - information content difference between the wildtype and mutant bases".
+  "cells - cell lines or tissues with evidence of chip-seq (human only) multiple cells are separated by 'and'" if defined $self->{tfbs};
    
   return \%header;
 }
@@ -210,8 +245,8 @@ sub parse_destabilizers {
     $item_value = substr $item_value, 2; 
 
     # get the value of the evidence
-    $evidence = undef $evidence == 0xFFFF;
-    my $evidence_val = defined $evidence ? $evidence_lval[$evidence] : undef;
+    $evidence = undef if $evidence == 0xFFFF;
+    $evidence_val = defined $evidence ? $evidence_lval[$evidence] : undef;
   }
   
   # get the rest
@@ -227,10 +262,70 @@ sub parse_destabilizers {
   return $dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd;
 }
 
-sub run {
+sub format_output{
+  my ($self, $data, $item) = @_;
+
+  my $result = {};
+
+  if ($self->{output_json}){
+    my %hash = map { $_ => $data->{$_} } @{ $field_order->{$item} };
+    $result->{$item} = \%hash;
+  }
+  else{
+    my $key = "mutfunc_" . $item;
+    $result->{$key} = join(",", map { $data->{$_} } @{ $field_order->{$item} });
+  }
+
+  return $result;
+}
+
+sub process_from_file {
+  my ($self, $tva) = @_;
+
+  my $vf = $tva->variation_feature;
+  
+  # get allele
+  my $allele = $tva->variation_feature_seq;
+  return {} unless $allele =~ /^[ACGT-]+$/;
+
+  # get matched line from the file
+  my @data =  @{$self->get_data($vf->{chr}, $vf->{start} - 2, $vf->{end})};
+
+  # parse data and generate output
+  my $result_from_file = {};
+  foreach (@data) {
+    my $matches = get_matched_variant_alleles(
+      {
+        ref    => $vf->ref_allele_string,
+        alts   => [$allele],
+        pos    => $vf->{start},
+        strand => $vf->strand
+      },
+      {
+       ref  => $_->{ref},
+       alts => [$_->{alt}],
+       pos  => $_->{start},
+      }
+    );
+
+    if (@$matches){
+      my $result = $_->{result}; 
+      
+      $result->{cells} =~ s/,/and/g;
+
+      # we are not expecting multiple results so no code for joiing multiple results
+      $result_from_file = $self->format_output($result, "tfbs");
+    }
+  }
+
+  return $result_from_file;
+}
+
+sub process_from_db {
   my ($self, $tva) = @_;
 
   # get the trascript related to the variant
+  return {} unless $tva->can('transcript');
   my $tr = $tva->transcript;
 
   # get the translation
@@ -250,7 +345,7 @@ sub run {
   }
   $self->{get_sth}->execute($md5);
 
-  my $result = {};
+  my $result_from_db = {};
   while (my $arrayref = $self->{get_sth}->fetchrow_arrayref) {
     my $item = $arrayref->[1];
     my $matrix = $arrayref->[2];
@@ -269,70 +364,126 @@ sub run {
     my $peptide_number = (first_index { $_ eq $peptide } @ALL_AAS);
 
     # get matrix for each item value and parse them
+    # motif
     if ($item eq "motif" && $self->{motif}) {
+      # get the item value for specific pos and amino acid from matrix
       my $tot_packed_len = 26;
       my $item_value = retrieve_item_value($expanded_matrix, $pos, $peptide_number, $tot_packed_len);
 
       if ($item_value){
-        my ($elm, $lost) = $parse_item_value->{$item}($item_value);
+        # parse the item value from matrix
+        my ($elm, $lost) = parse_motif($item_value);
 
-        $result->{mutfunc_motif} = $elm if defined $elm;
-        $result->{mutfunc_motif} .= defined $lost ? ",".$lost : ",";
+        # format the output
+        my $formatted_output = {};
+        if(defined $elm || defined $lost){
+          $formatted_output = $self->format_output({
+            elm   => $elm,
+            lost  => $lost
+          }, $item);
+        }
 
-        # delete if there is no defined value
-        delete $result->{mutfunc_motif} unless $result->{mutfunc_motif} =~ /[^,]/;
+        @$result_from_db{ keys %$formatted_output } = values %$formatted_output;
       }
     }
-    elsif ($item eq "int" && $self->{int}) {
-      my $tot_packed_len = 42;
-      my $item_value = retrieve_item_value($expanded_matrix, $pos, $peptide_number, $tot_packed_len);
-
-      if ($item_value){
-        my ($evidence, $dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd) = parse_destabilizers($item_value, $item);
-
-        # dG_mt can be calculated from dG_wt and ddG
-        my $dG_mt = (defined $dG_wt && defined $ddG) ? $dG_wt + $ddG : undef;
-
-        $result->{mutfunc_int} = $evidence if defined $evidence;
-        $result->{mutfunc_int} .= defined $dG_wt ? ",".$dG_wt : ",";
-        $result->{mutfunc_int} .= defined $dG_mt ? ",".$dG_mt : ",";
-        $result->{mutfunc_int} .= defined $ddG ? ",".$ddG : ",";
-        $result->{mutfunc_int} .= defined $dG_wt_sd ? ",".$dG_wt_sd : ",";
-        $result->{mutfunc_int} .= defined $dG_mt_sd ? ",".$dG_mt_sd : ",";
-        $result->{mutfunc_int} .= defined $ddG_sd ? ",".$ddG_sd : ",";
-
-        # delete if there is no defined value
-        delete $result->{mutfunc_int} unless $result->{mutfunc_int} =~ /[^,]/;
-      }
-    }
-    elsif ($item eq "mod" || $item eq "exp") {
-      if ($self->{$item}) {
-        my $tot_packed_len = 40;
+    # int and mod and exp
+    elsif ($item eq "int" || $item eq "mod" || $item eq "exp") {
+      if ($self->{$item}){
+        # get the item value for specific pos and amino acid from matrix
+        my $tot_packed_len = ($item eq "int") ? 42 : 40;
         my $item_value = retrieve_item_value($expanded_matrix, $pos, $peptide_number, $tot_packed_len);
 
         if ($item_value){
-          my ($dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd) = parse_destabilizers($item_value, $item);;
+          # parse the item value from matrix
+          my ($evidence, $dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd);
+          if ($item eq "int"){
+            ($evidence, $dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd) = parse_destabilizers($item_value, $item);
+          }
+          else{
+            ($dG_wt, $ddG, $dG_wt_sd, $dG_mt_sd, $ddG_sd) = parse_destabilizers($item_value, $item);
+          }
 
           # dG_mt can be calculated from dG_wt and ddG
           my $dG_mt = (defined $dG_wt && defined $ddG) ? $dG_wt + $ddG : undef;
 
-          my $hash_key = "mutfunc_" . $item;
-          $result->{$hash_key} = $dG_wt if defined $dG_wt;
-          $result->{$hash_key} .= defined $dG_mt ? ",".$dG_mt : ",";
-          $result->{$hash_key} .= defined $ddG ? ",".$ddG : ",";
-          $result->{$hash_key} .= defined $dG_wt_sd ? ",".$dG_wt_sd : ",";
-          $result->{$hash_key} .= defined $dG_mt_sd ? ",".$dG_mt_sd : ",";
-          $result->{$hash_key} .= defined $ddG_sd ? ",".$ddG_sd : ",";
+          # format the output
+          my $formatted_output = {};
+          if(defined $evidence || defined $dG_wt || defined $ddG || defined $dG_wt_sd || defined $dG_mt_sd || defined $ddG_sd){
+            my $data = {
+              dG_wt   => $dG_wt,
+              dG_mt  => $dG_mt,
+              ddG   => $ddG,
+              dG_wt_sd  => $dG_wt_sd,
+              dG_mt_sd   => $dG_mt_sd,
+              ddG_sd  => $ddG_sd
+            };
 
-          # delete if there is no defined value
-          delete $result->{$hash_key} unless $result->{$hash_key} =~ /[^,]/;
+            $data->{evidence} = $evidence if (defined $evidence && $item eq "int");
+
+            $formatted_output = $self->format_output($data, $item);
+          }
+
+          @$result_from_db{ keys %$formatted_output } = values %$formatted_output;
         }
       }
     }
-    
   }
 
-  return $result;
+  return $result_from_db;
+}
+
+sub run {
+  my ($self, $tva) = @_;
+  
+  my $result = {};
+
+  # parse data file and generate result for tfbs
+  if($self->{tfbs}){
+    my $hash_from_file = $self->process_from_file($tva);
+    @$result{ keys %$hash_from_file } = values %$hash_from_file;
+  }
+  # connect to db and generate result from matrix for the other type of analysis
+  if($self->{motif} || $self->{int} || $self->{mod} || $self->{exp}){
+    my $hash_from_db = $self->process_from_db($tva);
+    @$result{ keys %$hash_from_db } = values %$hash_from_db;
+  }
+
+  return $self->{output_json} ? {"mutfunc" => $result} : $result;
+}
+
+sub parse_data {
+  my ($self, $line) = @_;
+  my ($c, $s, $ref, $alt, $del, undef, undef, undef, undef, undef, 
+    $tf, $downstream, undef, undef, $g_strand, $s_strand, 
+    $wt_score, $mt_score, undef, $ic_diff, undef, 
+    $cells) = split /\t/, $line;
+  
+  return {
+    chr => $c,
+    start => $s,
+    end => $s,
+    ref => $ref,
+    alt => $alt,
+    result => {
+      impact => $del, 
+      tf => $tf, 
+      downstream => $downstream, 
+      g_strand => $g_strand,
+      s_strand => $s_strand, 
+      wt_score => $wt_score,
+      mt_score => $mt_score,
+      ic_diff => $ic_diff,
+      cells => $cells
+    }
+  };
+}
+
+sub get_start { 
+  return $_[1]->{start};
+}
+
+sub get_end {
+  return $_[1]->{end};
 }
 
 1;
