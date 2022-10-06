f50af236e90eb1ae0e4e0d36c783a37f988eecc7
olaaustine <olaaustine@ebi.ac.uk>
2022-10-06 16:04:47 +0100

    Fixed merge conflict

--- ProteinSeqs.pm
+++ ProteinSeqs.pm
@@ -92,10 +92,10 @@ sub run {
     # check if we have a mutant amino acid, if not there isn't much we can do!
 
     if (my $mut_aa = $tva->peptide) {
-        
-	return {} if !defined($tva->hgvs_protein);
-        
-	# get the peptide coordinates
+    
+      return {} if !defined($tva->hgvs_protein);
+      return {} if grep {$_->SO_term eq 'frameshift_variant'} @{$tva->get_all_OverlapConsequences};
+	  # get the peptide coordinates
 	
 	my $tl_start = $tva->transcript_variation->translation_start;
         my $tl_end = $tva->transcript_variation->translation_end;
