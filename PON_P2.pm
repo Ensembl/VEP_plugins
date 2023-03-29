f68b00e7048a333f62cc4563b7d1b4d197bedf8d
dglemos <dlemos@ebi.ac.uk>
2023-03-29 16:11:35 +0100

    Fix plugin PON_P2

--- PON_P2.pm
+++ PON_P2.pm
@@ -27,7 +27,6 @@ package PON_P2;
 use strict;
 use warnings;
 
-
 use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
 use Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin;
 
@@ -35,71 +34,71 @@ use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 
 
 sub feature_types {
-	return ['Transcript'];
+  return ['Transcript'];
 }
 
 
 sub get_header_info {
-	return {
-		PON_P2 => "PON-P2 prediction and score for amino acid substitutions"
-	};
+  return {
+    PON_P2 => "PON-P2 prediction and score for amino acid substitutions"
+  };
 }
 
 
 sub new {
-	my $class = shift;
-	my $self = $class->SUPER::new(@_);
-	# get parameters
-	my $command = $self->params->[0];
-	my $Hg = $self->params->[1];
-	die 'ERROR: Path to python script not specified! Specify path to python script e.g. --plugin PON_P2,/path/to/python/client/for/ponp2.py,[hg37/hg38]\n' unless defined($command);
-	die 'ERROR: Reference genome not specified! Specify the reference genome after the path to python file e.g. --plugin PON_P2,/path/to/python/client/for/ponp2.py,[hg37/hg38]\n' unless defined($command);
-	die "ERROR: Wrong reference genome specified! It should be either 'hg37' or 'hg38'\n" unless ($Hg ~~ ["hg37","hg38"]);
-	die 'ERROR: Incorrect path to ponp2.py\n' unless -e $command;
-	$self->{command} = $command;
-	$self->{Hg} = $Hg;
-	return $self;
+  my $class = shift;
+  my $self = $class->SUPER::new(@_);
+  # get parameters
+  my $command = $self->params->[0];
+  my $Hg = $self->params->[1];
+  die 'ERROR: Path to python script not specified! Specify path to python script e.g. --plugin PON_P2,/path/to/python/client/for/ponp2.py,[hg37/hg38]\n' unless defined($command);
+  die 'ERROR: Reference genome not specified! Specify the reference genome after the path to python file e.g. --plugin PON_P2,/path/to/python/client/for/ponp2.py,[hg37/hg38]\n' unless defined($command);
+  die "ERROR: Wrong reference genome specified! It should be either 'hg37' or 'hg38'\n" unless ($Hg ~~ ["hg37","hg38"]);
+  die 'ERROR: Incorrect path to ponp2.py\n' unless -e $command;
+  $self->{command} = $command;
+  $self->{Hg} = $Hg;
+  return $self;
 }
 
 
 sub run {
-	my ($self, $tva) = @_;
+  my ($self, $tva) = @_;
 
-	# only for missense variants
-	return {} unless grep {$_->SO_term eq 'missense_variant'} @{$tva->get_all_OverlapConsequences};
+  # only for missense variants
+  return {} unless grep {$_->SO_term eq 'missense_variant'} @{$tva->get_all_OverlapConsequences};
 
-	## Now get the variation features
-	my $vf=$tva -> variation_feature;
+  ## Now get the variation features
+  my $vf = $tva->variation_feature;
 
-	## If not snp return
-	return {} unless $vf->{start} == $vf->{end};
+  ## If not snp return
+  return {} unless $vf->{start} == $vf->{end};
 
-	my $Variation = $tva -> hgvs_genomic;
-	my ($Chr, $Pos, $Alt) = (split /:g.|>/, $Variation)[0,1,2];
-	my $Position = substr $Pos, 0, -1;
-	my $Ref = substr $Pos, -1;
-	
-	## Check for single nucleotide substitution
-	return {} unless $Ref =~ /^[ACGT]$/;
-	return {} unless $Alt =~ /^[ACGT]$/;
+  my $chr = $vf->{chr};
+  my $start = $vf->{start};
+  my $ref_allele = $vf->ref_allele_string;
+  my $alt_allele = $tva->base_variation_feature->alt_alleles;
 
-	my $command = $self -> {command};
-	my $Hg = $self -> {Hg};
-	my $V = $Chr."_".$Position."_".$Ref."_".$Alt;;
+  ## Check for single nucleotide substitution
+  return {} unless $Ref =~ /^[ACGT]$/;
+  return {} unless $Alt =~ /^[ACGT]$/;
 
-	## Call pon-p2 python script here
-	my $ponp2Res = `python $command $V $Hg` or return {};
-	$ponp2Res =~ s/\R//g;
+  my $command = $self->{command};
+  my $Hg = $self->{Hg};
+  my $V = $chr."_".$start."_".$ref_allele."_".$alt_allele[0];
 
-	my ($pred, $prob) =split /\t/, $ponp2Res;
+  ## Call pon-p2 python script here
+  my $ponp2Res = `python $command $V $Hg` or return {};
+  $ponp2Res =~ s/\R//g;
 
-	## Can PON-P2 predict?
-	return {} if $pred eq "cannot";
+  my ($pred, $prob) =split /\t/, $ponp2Res;
 
-	## Return predictions
-	return $pred && $prob ? {
-		PON_P2 => "$pred($prob)",
-	} : {};
+  ## Can PON-P2 predict?
+  return {} if $pred eq "cannot";
+
+  ## Return predictions
+  return $pred && $prob ? {
+    PON_P2 => "$pred($prob)",
+  } : {};
 }
 
 1;
