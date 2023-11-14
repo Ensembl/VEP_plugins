28f05cdb435d987929faec209e1c11b6690b682b
olaaustine <olaaustine@ebi.ac.uk>
2023-11-14 13:47:10 +0000

    Removing unneccessary modules

--- PhenotypeOrthologous.pm
+++ PhenotypeOrthologous.pm
@@ -58,8 +58,6 @@ package PhenotypeOrthologous;
 use strict;
 use warnings;
 
-use Bio::EnsEMBL::Utils::Sequence qw(reverse_comp);
-
 use Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin;
 
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepTabixPlugin);
