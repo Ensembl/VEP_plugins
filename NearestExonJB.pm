9a1e21b8a32934b28899dc80fa7b438152f168f1
dglemos <dlemos@ebi.ac.uk>
2025-01-30 17:58:10 +0000

    Change output

--- NearestExonJB.pm
+++ NearestExonJB.pm
@@ -58,6 +58,8 @@ use warnings;
 
 use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
 
+# use Data::Dumper;
+
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
 
 my $char_sep = "|";
@@ -107,6 +109,8 @@ sub run {
 
   my $loc_string = sprintf("%s:%s-%i-%i", $trv->transcript_stable_id, $vf->{chr} || $vf->seq_region_name, $vf->{start}, $vf->{end});
 
+  # print "\nlocation: $loc_string\n";
+
   if(!exists($self->{_cache}) || !exists($self->{_cache}->{$loc_string})) {
     my $exons = $trv->_overlapped_exons;
     my %dists;
@@ -114,9 +118,36 @@ sub run {
 
     if(scalar @{$exons} == 0 && $CONFIG{intronic} == 1) {
       $exons = $trv->_sorted_exons;
+      my $intron_numbers = $trv->intron_number();
+
+      if(defined $intron_numbers) {
+        my ($intron_number, $total_number) = split(/\//, $intron_numbers);
+        # print "Intron number: $intron_number\n";
+
+        # Get the number of exons before and after the intron
+        my $exon_before = $intron_number;
+        my $exon_after = $intron_number + 1;
+        # print "Exon before: $exon_before, exon after: $exon_after\n";
+
+        my @exons_tmp;
+        # Reverse strand we get the exons from the end of the list
+        if($tva->transcript->strand < 0) {
+          push(@exons_tmp, $exons->[-$exon_before]);
+          push(@exons_tmp, $exons->[-$exon_after]);
+        }
+        else {
+          push(@exons_tmp, $exons->[$exon_before -1]);
+          push(@exons_tmp, $exons->[$exon_after -1]);
+        }
+
+        $exons = \@exons_tmp;
+        # print "-> ", scalar(@{$exons}), "\n";
+      }
+
     }
 
     foreach my $exon (@$exons) {
+      # print "Exon: ", $exon->stable_id, " ", $exon->start, "-", $exon->end, "\n";
       my $startD = abs ($vf->start - $exon->seq_region_start);
       my $endD = abs ($vf->end - $exon->seq_region_end);
       if ($startD < $endD){
@@ -134,10 +165,23 @@ sub run {
       }
     }
 
+    # print "--- ", Dumper(\%dists);
+
     my @finalRes;
-    foreach my $exon (keys %dists){
-      if (exists $dists{$exon}{$min}) {
-        push(@finalRes, $exon.$char_sep.$min.$char_sep.$dists{$exon}{$min}.$char_sep.$dists{$exon}{len})
+    if(scalar @{$exons} == 2 && $CONFIG{intronic} == 1) {
+      # ENSE00003492822|2056|end|75
+      foreach my $exon (keys %dists) {
+        my $inner_hash = $dists{$exon};
+        my $inner_string = join($char_sep, map { "$_:$inner_hash->{$_}" } keys %$inner_hash);
+        my $string = $exon . $char_sep . $inner_string;
+        push(@finalRes, $string);
+      }
+    }
+    else {
+        foreach my $exon (keys %dists){
+        if (exists $dists{$exon}{$min}) {
+          push(@finalRes, $exon.$char_sep.$min.$char_sep.$dists{$exon}{$min}.$char_sep.$dists{$exon}{len})
+        }
       }
     }
 
@@ -147,4 +191,3 @@ sub run {
 }
 
 1;
-
